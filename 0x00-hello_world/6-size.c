bdbf762a5ef28d518c3e9ecc26a4a72626c3468e
pretzelogical <yingyangyo266@gmail.com>
2023-01-25 10:13:29 -0800

    change 6 to sate betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,22 @@
+/**
+ * main- prints the size of various types
+ * Return: returns 0
+ */
 #include <stdio.h>
 
 int main(void)
 {
-  char charType;
-  int intType;
-  long int longIntType;
-  long long longLongIntType;
-  float floatType;
-  
-  printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-  printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-  printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-  printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
-  printf("Size of a int: %zu byte(s)\n", sizeof(floatType));
-  
-  return(0);
+char charType;
+int intType;
+long int longIntType;
+long long longLongIntType;
+float floatType;
+
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+printf("Size of a int: %zu byte(s)\n", sizeof(floatType));
+
+return (0);
 }
