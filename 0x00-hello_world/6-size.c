2e1cb4c49f20b99bdd783153cf942b2c69b1b835
pretzelogical <yingyangyo266@gmail.com>
2023-01-25 10:15:29 -0800

    fix typo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
-printf("Size of a int: %zu byte(s)\n", sizeof(floatType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 }
