cdde875ee91e37dee98574b33a1507fd56596e75
pretzelogical <yingyangyo266@gmail.com>
2023-01-29 16:08:17 -0600

    add 5 & 6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign- prints the sign of a number
+ * @n: input number
+ * Description: Prints + if greater than 0, 0 if 0 and - if
+ * less than 0
+ *
+ * Return: 1 if greater than 0, 0 if 0 and -1 if less than 0
+ */
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+else
+{
+	_putchar('0');
+	return (0);
+}
+
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs- computes the absolute value of an integer
+ * @int: integer value to compute
+ *
+ * Return: absolute value of int
+ */
+int _abs(int)
+{
+
+if (x > 0)
+{
+	return (x);
+}
+else if (x < 0)
+{
+	return (x * -1);
+}
+else
+{
+	return (x);
+}
+
+}
