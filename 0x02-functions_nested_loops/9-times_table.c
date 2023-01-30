226e6e0a2b89cc768e8b575fddede98f9a5152b0
pretzelogical <yingyangyo266@gmail.com>
2023-01-30 11:39:19 -0600

    shorten 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,45 +8,36 @@
 void times_table(void)
 {
 int multiplier, num, output;
-
 for (multiplier = 0; multiplier < 10; multiplier++)
 	{
 	for (num = 0; num < 10; num++)
 		{
 		output =  multiplier * num;
+		/* if its in the first row dont put space*/
 		if (num == 0)
 			{
 			_putchar(output + '0');
-			_putchar(',');
-			_putchar(' ');
-			}
-		else if (num == 9 && output < 10)
-			{
-			_putchar(' ');
-			_putchar(output + '0');
-			_putchar('\n');
-			}
-		else if (num == 9 && output >= 10)
-			{
-			_putchar(((output / 10) % 10) + '0');
-			_putchar((output % 10) + '0');
-			_putchar('\n');
 			}
 		else if (output < 10)
 			{
 			_putchar(' ');
 			_putchar(output + '0');
-			_putchar(',');
-			_putchar(' ');
 			}
 		else if (output >= 10)
 			{
 			_putchar(((output / 10) % 10) + '0');
 			_putchar((output % 10) + '0');
+			}
+
+		if (num < 9)
+			{
 			_putchar(',');
 			_putchar(' ');
 			}
+		else
+			{
+			_putchar('\n');
+			}
 		}
 	}
-
 }
