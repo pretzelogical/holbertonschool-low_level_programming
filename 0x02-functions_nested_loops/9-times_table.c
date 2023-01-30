6814d6793c33ff2baf3b126989c02391b7d4cfd5
pretzelogical <yingyangyo266@gmail.com>
2023-01-30 11:20:54 -0600

    add 8 & 9 in 0x02

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * times_table- print the 9 times table
+ *
+ * Return: Always return nothing (void)
+ */
+void times_table(void)
+{
+int multiplier, num, output;
+
+for (multiplier = 0; multiplier < 10; multiplier++)
+	{
+	for (num = 0; num < 10; num++)
+		{
+		output =  multiplier * num;
+		if (num == 0)
+			{
+			_putchar(output + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+		else if (num == 9 && output < 10)
+			{
+			_putchar(' ');
+			_putchar(output + '0');
+			_putchar('\n');
+			}
+		else if (num == 9 && output > 10)
+			{
+			_putchar(((output / 10) % 10) + '0');
+			_putchar((output % 10) + '0');
+			_putchar('\n');
+			}
+		else if (output < 10)
+			{
+			_putchar(' ');
+			_putchar(output + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+		else if (output > 10)
+			{
+			_putchar(((output / 10) % 10) + '0');
+			_putchar((output % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int x);
 int print_last_digit(int x);
+void jack_bauer(void);
+void times_table(void);
+
 
 #endif
 
