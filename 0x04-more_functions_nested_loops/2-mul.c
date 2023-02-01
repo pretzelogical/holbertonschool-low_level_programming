4175b860427de8f188b6f1d420df3a3d0d602f1c
pretzelogical <yingyangyo266@gmail.com>
2023-02-01 11:08:48 -0600

    Add 1-5 to 0x04

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul- multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a & b multiplied
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
