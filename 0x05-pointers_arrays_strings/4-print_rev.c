2453e438f6f5533708bb30b2f645c868d7affcce
pretzelogical <yingyangyo266@gmail.com>
2023-02-03 13:25:11 -0600

    start 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_rev- Prints a string in reverse
+ * followed by a newline
+ * @s: string to print in reverse
+ *
+ * Return: Always return nothing (void)
+ */
+void print_rev(char *s)
+{
+
+}
