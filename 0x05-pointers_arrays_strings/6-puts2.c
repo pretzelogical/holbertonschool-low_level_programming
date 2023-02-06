708b79bb10cf00726edc7a8151c1d101fa7d4599
pretzelogical <yingyangyo266@gmail.com>
2023-02-05 19:29:04 -0600

    add 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * puts2- print every other character of a string
+ * followed by a newline
+ * @str: string to print
+ *
+ * Return: Always return nothing (void)
+ */
+void puts2(char *str)
+{
+int len, i;
+
+i = 0;
+len = _strlen(str) - 1;
+
+while ( i < len )
+	{
+	if ( i % 2 == 0 )
+		{
+		_putchar(*str);
+		}
+	str++;
+	i++;
+	}
+_putchar('\n');
+}
+
+/**
+ * _strlen- returns the length of a string
+ * @s: string to check lenght of
+ *
+ * Return: Always return length of string
+ */
+int _strlen(char *s)
+{
+int l;
+
+l = 0;
+while (*s != '\0')
+	{
+	s++;
+	l++;
+	}
+
+return (l);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "header.h"
+
+/**
+ * main- Describe main function
+ * @x: Describe function argument
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+/* code here */
+return (0);
+}
+
