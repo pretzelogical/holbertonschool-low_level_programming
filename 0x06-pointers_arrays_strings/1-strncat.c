e0bc452c6bb5e189150cd7d21843cfeb37803a08
pretzelogical <yingyangyo266@gmail.com>
2023-02-08 12:33:49 -0600

    added 1 (1 SEGFAULTS)

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * _strncat- concatenates two strings using n bytes
+ * from src
+ * @dest: string to append to
+ * @src: string to append
+ * @n: how many bytes to copy
+ *
+ * Return: Always return a pointer to the resulting
+ * string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, destlen, srclen;
+
+	destlen = _strlen(dest);
+	srclen = _strlen(src);
+	i = destlen;
+	/* starts at nullbyte of dest*/
+	while (i < destlen + n)
+		{
+		*(dest + i) = *(src + (i - destlen));
+		i++;
+		}
+
+	if (srclen <= n)
+		*(dest + i) = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen- returns the length of a string
+ * @s: string to check length of
+ *
+ * Return: Always return length of string
+ */
+int _strlen(char *s)
+{
+int l;
+
+l = 0;
+while (*s != '\0')
+	{
+	s++;
+	l++;
+	}
+
+return (l);
+}
