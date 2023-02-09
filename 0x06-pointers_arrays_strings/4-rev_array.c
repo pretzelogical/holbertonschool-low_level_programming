011953e9774581276f6e6aa10ec8d5e512696832
pretzelogical <yingyangyo266@gmail.com>
2023-02-09 11:55:55 -0600

    Added 4 in 0x06

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * reverse_array- reverses the content of an array
+ * of integers
+ * @a: array to reverse
+ * @n: number of elements in array
+ *
+ * Return: Always return nothing (void)
+*/
+void reverse_array(int *a, int n)
+{
+	int *start, *end, tmp;
+
+	start = a;
+	end = a + (n - 1);
+	tmp = 0;
+
+	while (start != end)
+	{
+		tmp = *start;
+		*start = *end;
+		*end = tmp;
+		start++;
+		end--;
+	}
+}
