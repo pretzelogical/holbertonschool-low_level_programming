36bd8ca5ce1831bc041f705681c5817043d06a1b
pretzelogical <yingyangyo266@gmail.com>
2023-02-10 11:08:26 -0600

    fix 7 formatting

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,30 +2,29 @@
 
 /**
  * leet- encodes a string into 1337SP34K
- * @s- string to encode
- * 
+ * @s: string to encode
+ *
  * Return: Always return a pointer to the string
 */
 char *leet(char *s)
-{	
+{
 	char *start;
 
 	start = s;
-	
+
 	while (*s != '\0')
 	{
 		leet_convert(s);
 		s++;
 	}
-
 	return (start);
 
 }
 
 /**
  * leet_convert- changes character to 1337 version
- * @c- character to check
- * 
+ * @c: character to check
+ *
  * Return: Return converted character, else 0
 */
 char leet_convert(char *c)
@@ -36,7 +35,7 @@ char leet_convert(char *c)
 
 
 	for (i = 0; i < 5; i++)
-		if(*c == pre[i] || *c == pre[i] + 32)
+		if (*c == pre[i] || *c == pre[i] + 32)
 		{
 
 			*c = post[i];
