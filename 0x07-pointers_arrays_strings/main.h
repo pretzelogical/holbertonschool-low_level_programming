684516ede2366ec8a839ea46dee959ed22b30c67
pretzelogical <yingyangyo266@gmail.com>
2023-02-10 14:05:10 -0600

    add 3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char is_subst_match(char *c, char *subst);
 
 #endif
