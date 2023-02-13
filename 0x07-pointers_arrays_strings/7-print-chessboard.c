86d9dd815aaf02da1a97c844138a0345b878e6fd
pretzelogical <yingyangyo266@gmail.com>
2023-02-13 13:38:17 -0600

    add 5 and start 7

---
+++ 0x07-pointers_arrays_strings/7-print-chessboard.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_chessboard- prints the chessboard
+ * @a: board to print
+ *
+ * Return: Always return nothing (void)
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i;
+
+	
+}
