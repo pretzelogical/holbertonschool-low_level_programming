dc8dc733064fd604298b939920e662dd5bbedbdd
pretzelogical <yingyangyo266@gmail.com>
2023-02-13 14:59:11 -0600

    Add 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums- print the sum of two diagonals of
+ * a square matrix of integers
+ * @a: array
+ * @size: size of array
+ *
+ * Return: Always return nothing (void)
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, n1, n2;
+
+	n1 = 0;
+	n2 = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		n1 += a[(i * size) + i];
+		n2 += a[(size - 1) + ((size - 1) * i)];
+	}
+	printf("%d, %d\n", n1, n2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _strlen(char *s);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif
