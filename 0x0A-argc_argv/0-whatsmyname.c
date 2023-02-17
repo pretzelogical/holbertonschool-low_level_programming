30d68ca90e20a7ea2eed938842a6e5e65ca0d009
pretzelogical <yingyangyo266@gmail.com>
2023-02-17 12:34:33 -0600

    add attribute ((unused)) to 0 & 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,12 +7,8 @@
  *
  * Return: Always return 0 (success)
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
-
-	if (argc == 1)
-		printf("%s\n", argv[argc - 1]);
-	else
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
