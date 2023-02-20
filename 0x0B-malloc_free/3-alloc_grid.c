0a2d629ce3464c0d21ce7ec217897a5297b6a280
pretzelogic <yingyangyo266@gmail.com>
2023-02-20 14:10:39 -0600

    change 3 method

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,17 +14,16 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(sizeof(int *) * height);
-	if (grid == NULL)
-		return (NULL);
+	grid = malloc(width * sizeof(int *));
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * width);
+		grid[i] = malloc(height * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 				free(grid[j]);
 			free(grid);
+			return (NULL);
 		}
 	}
 
