4ae4e9f81724a576727f7efc46df7fe07d0bcd1a
pretzelogic <yingyangyo266@gmail.com>
2023-02-20 16:03:15 -0600

    revert 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,12 +14,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = malloc(sizeof(int *) * width);
+	grid = (int **)malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = malloc(sizeof(int) * height);
+		grid[i] = (int *)malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -28,8 +28,8 @@ int **alloc_grid(int width, int height)
 		}
 	}
 
-	for (i = 0; i < width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 
 	return (grid);
