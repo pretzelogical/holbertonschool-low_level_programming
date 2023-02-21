b9e0ba39af11c22769c52606ec0a10f5a635364c
pretzelogic <yingyangyo266@gmail.com>
2023-02-20 21:38:59 -0600

    change 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,18 +10,10 @@
 */
 void free_grid(int **grid, int height)
 {
+
 	int i;
 
-	if (height <= 0)
-		return;
-
-	if (grid == NULL)
-		return;
-
 	for (i = 0; i < height; i++)
-	{
 		free(grid[i]);
-	}
-
 	free(grid);
 }
