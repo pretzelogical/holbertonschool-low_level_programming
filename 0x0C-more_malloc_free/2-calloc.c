d2d38acb83c47cbef546caabaef26fdf13ae7124
pretzelogical <yingyangyo266@gmail.com>
2023-02-26 08:50:59 -0600

    update 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *arr;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
