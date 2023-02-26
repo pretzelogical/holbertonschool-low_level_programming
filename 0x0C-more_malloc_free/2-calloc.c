c313ad235f34e2eff71d77585d47e18c0b9af380
pretzelogical <yingyangyo266@gmail.com>
2023-02-26 09:02:06 -0600

    add 0 initialisation with size * nmemb method

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,15 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *arr;
+	char *arr;
+	unsigned int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+	for (i = 0; i < (size * nmemb); i++)
+		arr[i] = 0;
 	return (arr);
 }
