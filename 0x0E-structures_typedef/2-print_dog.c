2a983c62626cad8f052e6856098768c024b9357a
pretzelogic <yingyangyo266@gmail.com>
2023-03-02 09:35:02 -0600

    fix fornmatting

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,14 +13,15 @@ void print_dog(struct dog *d)
 		return;
 
 	if (!d->name)
-		printf("Name : (nil)\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	
-	printf("Age: %f\n", d->age);
-
+	if (!d->age)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 	if (!d->owner)
-		printf("Owner : (nil)\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
