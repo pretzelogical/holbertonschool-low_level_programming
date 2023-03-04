2f659b84cb446eba04420444da9068ebf448142a
pretzelogical <yingyangyo266@gmail.com>
2023-03-04 14:08:44 -0600

    start 3 in 0x0F

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add- returns the sum of a & b
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: a + b
+*/
+int op_add(int a, int b)
+{
+	return a + b;
+}
+/**
+ * op_sub returns the difference
+ * of a & b
+ * @a: first number to subtract
+ * @b: second number to subtract
+ *
+ * Return: a - b
+*/
+int op_sub(int a, int b)
+{
+	return a - b;
+}
+/**
+ * op_mul- returns the product
+ * of a & b
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: a * b
+*/
+int op_add(int a, int b)
+{
+	return a * b;
+}
+/**
+ * op_div- returns the result
+ * of division of a & b
+ * @a: first number to divide
+ * @b: second number to divide
+ *
+ * Return: a / b
+*/
+int op_add(int a, int b)
+{
+	return a / b;
+}
+/**
+ * op_mod- returns the remainder
+ * of division of a & b
+ * @a: first number to modulo
+ * @b: second number to modulo
+ *
+ * Return: a % b
+*/
+int op_add(int a, int b)
+{
+	return a % b;
+}
