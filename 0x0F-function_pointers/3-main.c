135d340577dd6a4dea0840262f53f01f22c09a17
pretzelogical <yingyangyo266@gmail.com>
2023-03-05 13:02:16 -0600

    3 second version (add op string length check)

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	op = argv[2];
 	y = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
