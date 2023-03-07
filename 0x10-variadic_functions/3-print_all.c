5e28b9af4c41bfd6daa82c291d631fe8354063b3
pretzelogical <yingyangyo266@gmail.com>
2023-03-06 20:43:56 -0600

    remove _strlen

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,24 +1,5 @@
 #include "variadic_functions.h"
 
-/**
- * _strlen- returns the length of a string
- * @s: string to check lenght of
- *
- * Return: Always return length of string
- */
-int _strlen(char *s)
-{
-int l;
-
-l = 0;
-while (*s != '\0')
-	{
-	s++;
-	l++;
-	}
-
-return (l);
-}
 
 /**
  * print_all- prints anything
