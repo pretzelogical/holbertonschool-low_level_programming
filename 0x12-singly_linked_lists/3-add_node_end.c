9a9791bb488ce9f8c7827c18a1f6f4aa3692b59d
pretzelogical <yingyangyo266@gmail.com>
2023-03-16 15:49:59 -0500

    free new if str fails in add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -39,7 +39,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		new->str = strdup(str);
 		if (!new->str)
+		{
+			free(new);
 			return(NULL);
+		}
 		new->len = _strlen(str);
 	}
 	new->next = NULL;
