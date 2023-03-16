7b7af059d879e679835246ab7b3f94a057a5df70
pretzelogical <yingyangyo266@gmail.com>
2023-03-16 16:00:53 -0500

    add free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_list- frees a singly linked list
+ * @head: head of list to free
+ *
+ * Return: Void return
+*/
+void free_list(list_t *head)
+{
+	if (head->next)
+		free_list(head->next);
+	free(head->str);
+	free(head);
+}
