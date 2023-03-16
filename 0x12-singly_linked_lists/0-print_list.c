fe94ee720c2c03bd78db34ed1b8b16832ac36885
pretzelogical <yingyangyo266@gmail.com>
2023-03-16 12:17:21 -0500

    start 0x12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list- prints all the elements of a
+ * list_t list
+ * @h: list to print elements of
+ *
+ * Return: The number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	
+}
