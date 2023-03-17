3f6e460ba00055572be78eaffa9e557c97e9f2e5
pretzelogic <yingyangyo266@gmail.com>
2023-03-17 08:53:01 -0500

    add binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _pow- returns x raised to exponent
+ * @x: number to raise
+ * @exponent: power to raise to
+ *
+ * Return: x raised to exponent
+*/
+int _pow(int x, int exponent)
+{
+	for (; exponent; exponent--)
+		x *= x;
+	return (x);
+}
+
+/**
+ * _strlen- returns the length of a string
+ * @s: string to check lenght of
+ *
+ * Return: Always return length of string
+ */
+int _strlen(const char *s)
+{
+int l;
+
+l = 0;
+while (*(s + l) != '\0')
+	l++;
+
+return (l);
+}
+
+/**
+ * binary_to_uint- converts a string of 1 & 0 chars
+ * to an unsigned int
+ * @b: string to convert
+ *
+ * Return: The converted number or 0 if string is invalid
+ * or null
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int out;
+	int outl, mult;
+
+	out = 0;
+	
+	if (!b)
+		return (out);
+	outl = _strlen(b);
+
+	mult = 1;
+	for (outl -= 1; outl >= 0; outl--)
+	{
+		if (b[outl] != '0' && b[outl] != '1')
+			return (0);
+		out += (b[outl] - '0') * mult;
+		mult *= 2;
+	}
+	return (out);
+}
