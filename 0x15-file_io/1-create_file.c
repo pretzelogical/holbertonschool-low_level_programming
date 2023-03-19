11f8dd44ba3e84f9534e0de79ffaefe4f90b3aa8
pretzelogic <yingyangyo266@gmail.com>
2023-03-19 16:19:18 -0500

    change order of open flags

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -53,7 +53,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fileD = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fileD = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fileD == -1)
 		return (-1);
 
@@ -63,14 +63,14 @@ int create_file(const char *filename, char *text_content)
 	str = malloc(sizeof(char) * _strlen(text_content) + 1);
 	if (!str)
 		return (-1);
+
 	_strcpy(str, text_content);
-	str[_strlen(text_content) + 1] = '\0';
 	wr = write(fileD, str, _strlen(text_content) + 1);
 	if (wr == -1)
 		return (-1);
+
+
 	free(str);
-
-
 	close(fileD);
 	return (1);
 }
