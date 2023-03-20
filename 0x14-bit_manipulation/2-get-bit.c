7bdfedf4828649beb00106f3acd10cff8f74a54e
pretzelogic <yingyangyo266@gmail.com>
2023-03-20 11:26:06 -0500

    add get_bit

---
+++ 0x14-bit_manipulation/2-get-bit.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _count_bits- count the bit places in a given number
+ * @n: number to count bit places
+ *
+ * Return: number of bit places in n
+*/
+int _count_bits(unsigned long int n)
+{
+	int count;
+
+	count = 0;
+	while (n != 0)
+	{
+		count++;
+		n = n >> 1;
+	}
+	return (count);
+}
+
+/**
+ * get_bit- gets the value of a bit at a given index
+ * @n: number to get bit from
+ * @index: index starting from 0 of the bit you want to get
+ *
+ * Return: The value of the bit at index or -1 if fail
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int mask, count;
+
+	count = _count_bits(n);
+
+	if ((int) index > count)
+		return (-1);
+
+	mask = (1 << index);
+
+	if (n & mask)
+		return (1);
+	else
+		return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,7 @@ unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
 int _pow(int x, int exponent);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int _count_bits(unsigned long int n);
 
 #endif
