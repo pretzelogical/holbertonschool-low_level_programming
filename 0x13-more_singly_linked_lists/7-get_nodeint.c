36a2d21c22a4294054660dc4340798838acfa4d4
pretzelogical <yingyangyo266@gmail.com>
2023-03-20 23:43:26 -0500

    add null check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: head of the listint_t list
  * @index: index of the node
  *
- * Return: address of node
+ * Return: address of node else NULL
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
@@ -15,7 +15,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (head);
 
 	for (i = 0; i < index; i++)
+	{
 		head = head->next;
-
+		if (!head)
+			return (NULL);
+	}
 	return (head);
 }
