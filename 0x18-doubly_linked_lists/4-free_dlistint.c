171b07c45b74a4895e1af8dc53cc8ad9ccbd16de
pretzelogical <yingyangyo266@gmail.com>
2023-03-23 13:08:06 -0500

    Add free_dlistint

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_dlistint- frees a dlistint_t list
+ * @head: head of list
+ *
+ * Return: void return
+*/
+void free_dlistint(dlistint_t *head)
+{
+	if (head->next)
+		free_dlistint(head->next);
+	free(head);
+}
