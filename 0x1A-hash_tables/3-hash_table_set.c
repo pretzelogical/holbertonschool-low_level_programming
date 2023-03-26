94ad4d43393dd1a5ae87dfb92a52c34457b29c4d
pretzelogical <yingyangyo266@gmail.com>
2023-03-26 17:13:57 -0500

    add key_index & start hash_table_set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set- adds an element to the hash table
+ * @ht: hash table you want to add or update key/value to
+ * @key: the key, cannot be an empty string
+ * @value: the value associated with the key, can be an empty string
+ *
+ * Return: 1 if succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	
+}
