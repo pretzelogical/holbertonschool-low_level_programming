2ddedd7b33be2bc993ea2502a0747a09dc623aa3
pretzelogical <yingyangyo266@gmail.com>
2023-03-26 17:02:56 -0500

    add hash_table_create and djb2

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create- creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to new hash table else NULL
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned long int i;
+
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (NULL);
+
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		new->array = NULL;
+
+	return (new);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,2 @@
+0x1A hash tables
+Hash tables are data structures that are operated on by functions
