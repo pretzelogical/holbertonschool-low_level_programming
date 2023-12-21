869fd6d2ed510d889c8e7d66da8020759f127648
pretzelogical <yingyangyo266@gmail.com>
2023-12-21 13:31:46 -0600

    try fix checker

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,6 +13,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i < size - 1; i++)
 	{
 		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
