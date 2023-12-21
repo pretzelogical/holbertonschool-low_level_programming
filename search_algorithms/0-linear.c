d80cffc7d48f0fa9ad2ea04b18373cdea76ee975
pretzelogical <yingyangyo266@gmail.com>
2023-12-21 13:37:29 -0600

    move printf

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -18,9 +18,9 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 	}
 	return (-1);
 }
